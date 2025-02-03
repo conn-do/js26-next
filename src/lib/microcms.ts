dd2142ac785f94353d11fb9145633b188e29c008
conn-do <kondo@japaan.jp>
2025-02-04 02:02:59 +0900

    Fix apiKey

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -2,7 +2,7 @@ import { createClient } from 'microcms-js-sdk'
 
 export const client = createClient({
   serviceDomain: 'hal-js',
-  apiKey: '0hnEA8jSd47RZ1dAekoM9MK4qs538D5LbDLy',
+  apiKey: process.env.MICROCMS_API_KEY ?? '',
 })
 
 export type Blog = {
